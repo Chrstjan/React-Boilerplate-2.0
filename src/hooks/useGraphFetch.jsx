a9d56c9815acb2f1e461ad143bacbfb9ccd521d2
Christian <ps5christian@hotmail.com>
2024-09-27 17:11:05 +0200

    Start of the new boilerplate

---
+++ src/hooks/useGraphFetch.jsx
@@ -0,0 +1,13 @@
+import { useQuery } from "@tanstack/react-query";
+import { request } from "graphql-request";
+
+export const useGraphFetch = (url, envKey, query) => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: [query],
+    queryFn: async () => request(import.meta.env[envKey], query),
+    staleTime: 600 * 10, //data is fresh for 1 minute,
+    cacheTime: 3000 * 10, //In cache for 5 minutes
+    retry: 1, //Retry fetch once if fail on the first fetch
+  });
+  return { data, isLoading, error };
+};
