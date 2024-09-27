ee16abdcb392579b0d609165608a1e313ef2bbd5
Christian <ps5christian@hotmail.com>
2024-09-27 22:19:49 +0200

    Made new fetch type + made graph fetch more flexible

--- src/hooks/useGraphFetch.jsx
+++ src/hooks/useGraphFetch.jsx
@@ -4,7 +4,7 @@ import { request } from "graphql-request";
 export const useGraphFetch = (url, envKey, query) => {
   const { data, isLoading, error } = useQuery({
     queryKey: [query],
-    queryFn: async () => request(import.meta.env[envKey], query),
+    queryFn: async () => request(url || import.meta.env[envKey], query),
     staleTime: 600 * 10, //data is fresh for 1 minute,
     cacheTime: 3000 * 10, //In cache for 5 minutes
     retry: 1, //Retry fetch once if fail on the first fetch
