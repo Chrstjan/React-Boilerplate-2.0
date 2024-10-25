bc3a7bbc776da1da1ce48afe47bad8bc04735c95
Chrstjan <ps5christian@hotmail.com>
2024-10-25 10:04:46 +0200

    Started making reusable components

--- src/router/PageRouter.jsx
+++ src/router/PageRouter.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import { Paths } from "./Paths";
 import { MainLayout } from "../Layouts/MainLayout";
+import { LandingPage } from "../pages/LandingPage";
+import { PageNotFound } from "../pages/PageNotFound";
 
 export const PageRouter = () => {
   return (
     <Routes>
       <Route path={Paths.home} element={<MainLayout />}>
-        <Route index element={<h1>React Boilerplate 2.0</h1>} />
-        <Route path={Paths.pageNotFound} element={<h2>404 not found...</h2>} />
+        <Route index element={<LandingPage />} />
+        <Route path={Paths.pageNotFound} element={<PageNotFound />} />
       </Route>
     </Routes>
   );
